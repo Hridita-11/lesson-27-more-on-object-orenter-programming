0890cbcd006cf74bf89a991eb5968703f3303177
Hridita-11 <hridita.nz@gmail.com>
2026-05-19 18:22:44 +1200

    Add files via upload

---
+++ string_upper_case.py
@@ -0,0 +1,10 @@
+class IOSstring:
+    def _init_(self): 
+         self.str1=""
+    def get_String(self):
+        self.str1=input("Enter a string:")
+    def print_String(self):
+         print("Result is:", self.str1.upper())
+str1=IOSstring()
+str1.get_String()
+str1.print_String()
