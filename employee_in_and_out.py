15010d6a7732196ecabe93c090c2a99a28ffebff
Hridita-11 <hridita.nz@gmail.com>
2026-05-19 18:33:59 +1200

    Add files via upload

---
+++ employee_in_and_out.py
@@ -0,0 +1,16 @@
+class Employee:
+    def _init_(self):
+        print("Employee created:")
+    def _del_(self):
+        print("Destructor called")
+def create_obj():
+        print("Making object...")
+        obj=Employee()
+        print("Function end:")
+        return obj
+print ("Calling create_obj()function...")
+obj= create_obj()
+print("Program End")
+
+        
+          
